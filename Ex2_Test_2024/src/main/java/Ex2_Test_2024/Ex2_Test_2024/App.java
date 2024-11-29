2bf09f1c44e62ec1da4aa5a1b1dface860fe9a48
Isl-n <gazi.ign@gmail.com>
2024-11-29 07:22:38 +0200

    Ex2

---
+++ Ex2_Test_2024/src/main/java/Ex2_Test_2024/Ex2_Test_2024/App.java
@@ -0,0 +1,10 @@
+package Ex2_Test_2024.Ex2_Test_2024;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Ex2_Test_2024/src/test/java/Ex2_Test_2024/Ex2_Test_2024/AppTest.java
@@ -0,0 +1,19 @@
+package Ex2_Test_2024.Ex2_Test_2024;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
